b876b4ef7fcb5c30aa049860566cad9a52e9d26a
Thomas GREVEDON <thomas.grevedon@sii.fr>
2021-10-13 21:34:03 +0200

    chore(firebase): adding database config to project

---
+++ src/configs/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+require('dotenv').config({ debug: process.env.DEBUG });
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+export default database;
