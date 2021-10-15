70e7c1f077d0364a50cf892ef7513d3e17b9d71d
Florian Duquesne <fduquesne@icloud.com>
2021-10-15 16:44:38 +0200

    ui(auth): add form title

--- src/views/AuthenticationView/AuthentificationForm/index.vue
+++ src/views/AuthenticationView/AuthentificationForm/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="auth-form">
+    <div class="title">{{ title }}</div>
     <slot />
     <Button :label="submitLabel" color="christmas" size="big" @click="submit" />
   </div>
@@ -14,12 +15,21 @@
     props: {
       submit: { type: Function, required: true },
       submitLabel: { type: String, required: true },
+      title: { type: String, required: true },
     },
   };
 </script>
 
 <style lang="scss" scoped>
+  @import '@/assets/scss/_colors.scss';
+
   .auth-form {
     width: 400px;
+
+    .title {
+      padding-bottom: 30px;
+      color: $color-christmas-yellow-dark;
+      font-size: 40px;
+    }
   }
 </style>
--- src/views/AuthenticationView/Signup.vue
+++ src/views/AuthenticationView/Signup.vue
@@ -1,5 +1,6 @@
 <template>
-  <AuthentificationForm submitLabel="Créer son compte" :submit="createUserAccount">
+  <AuthentificationForm title="Inscription" submitLabel="Créer son compte" :submit="createUserAccount">
+    <Input type="text" label="Code projet" size="big" @update="projectCode = $event" />
     <Input type="text" label="Nom utilisateur" placeholder="Père noël" size="big" @update="username = $event" />
     <Input type="password" label="Mot de passe" placeholder="••••••••••••" size="big" @update="password = $event" />
     <Input
@@ -22,6 +23,7 @@
     components: { Input, AuthentificationForm },
     data() {
       return {
+        projectCode: '',
         username: '',
         password: '',
         confirmPassword: '',
@@ -29,11 +31,12 @@
     },
     methods: {
       createUserAccount() {
+        // TODO: Check data entered by user
         // TODO: Call actions to create a user account
-        // this.$store.dispatch('createUserAccount', this.username, this.password);
         // TODO: Call actions to authenticate user
+
         // Redirect user to the homepage
-        // this.$router.push('/');
+        this.$router.push('/');
       },
     },
   };
