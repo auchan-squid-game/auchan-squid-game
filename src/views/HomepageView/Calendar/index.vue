a2db3bf4b1bbf5a7abbf7b1a14931bc90d8992cd
Florian Duquesne <fduquesne@icloud.com>
2021-10-16 00:14:38 +0200

    ui(homepage): flip cards

--- src/views/HomepageView/Calendar/index.vue
+++ src/views/HomepageView/Calendar/index.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="calendar-container">
     <div id="calendar">
-      <CalendarCell id="cell-2" day="2" />
-      <CalendarCell id="cell-21" day="21" />
-      <CalendarCell id="cell-9" day="9" />
-      <CalendarCell id="cell-1" day="1" centeredIcon="snowflake" />
+      <CalendarCell id="cell-1" day="1" enable centeredIcon="snowflake" />
+      <CalendarCell id="cell-2" day="2" enable />
+      <CalendarCell id="cell-3" day="3" enable />
+      <CalendarCell id="cell-6" day="6" enable text="Bonnes vacances" direction="horizontal" />
+      <CalendarCell id="cell-7" day="7" enable centeredIcon="moon-stars" />
+      <CalendarCell id="cell-8" day="8" enable />
+      <CalendarCell id="cell-9" day="9" enable />
       <CalendarCell id="cell-10" day="10" />
-      <CalendarCell id="cell-3" day="3" />
-      <CalendarCell id="cell-22" day="22" icon="sugar-cane" iconPosition="right" />
       <CalendarCell id="cell-13" day="13" centeredIcon="christmas-tree" />
-      <CalendarCell id="cell-23" day="23" />
       <CalendarCell id="cell-14" day="14" icon="christmas-leaves" iconPosition="right" />
-      <CalendarCell id="cell-20" day="20" />
-      <CalendarCell id="cell-8" day="8" />
-      <CalendarCell id="cell-17" day="17" icon="christmas-socks" iconPosition="left" />
-      <CalendarCell id="cell-6" day="6" text="Bonnes vacances" direction="horizontal" />
-      <CalendarCell id="cell-7" day="7" centeredIcon="moon-stars" />
-      <CalendarCell id="cell-24" day="24" text="Joyeux Noël" direction="vertical" />
       <CalendarCell id="cell-15" day="15" />
       <CalendarCell id="cell-16" day="16" />
+      <CalendarCell id="cell-17" day="17" icon="christmas-socks" iconPosition="left" />
+      <CalendarCell id="cell-20" day="20" />
+      <CalendarCell id="cell-21" day="21" />
+      <CalendarCell id="cell-23" day="23" />
+      <CalendarCell id="cell-22" day="22" icon="sugar-cane" iconPosition="right" />
+      <CalendarCell id="cell-24" day="24" text="Joyeux Noël" direction="vertical" />
     </div>
   </div>
 </template>
@@ -48,43 +48,20 @@
       width: 100%;
       height: 100%;
 
-      // DOTS
-      #cell-21 {
-        grid-area: 1 / 2 / 1 / 4;
-        background-color: $color-christmas-red;
-        background-image: radial-gradient($color-christmas-red-dark 12%, transparent 0),
-          radial-gradient($color-christmas-red-dark 12%, transparent 0);
-        background-size: 20px 20px;
-        background-position: 0 0, (20px / 2) (20px / 2);
-        color: $color-white;
-      }
+      // CENTERED ICON
+      #cell-1 {
+        :deep(.calendar-cell) {
+          background-color: $color-background-light;
+          color: $color-white;
 
-      // DIAGONAL
-      #cell-9 {
-        grid-area: 1 / 4 / 3 / 4;
-        background: repeating-linear-gradient(
-          -45deg,
-          $color-christmas-yellow,
-          $color-christmas-yellow 20px,
-          $color-christmas-green 20px,
-          $color-christmas-green 40px
-        );
-      }
-
-      // ZIGZAG
-      #cell-10 {
-        grid-area: 1 / 6 / 1 / 8;
-        background: linear-gradient(135deg, $color-christmas-red 25%, transparent 25%) -25px 0,
-          linear-gradient(225deg, $color-christmas-red 25%, transparent 25%) -25px 0,
-          linear-gradient(315deg, $color-christmas-red 25%, transparent 25%),
-          linear-gradient(45deg, $color-christmas-red 25%, transparent 25%);
-        background-size: 50px 50px;
-        background-color: $color-christmas-red-dark;
-        color: $color-white;
+          .centered-icon {
+            color: $color-background-very-light;
+          }
+        }
       }
 
       // STARS
-      #cell-2 {
+      #cell-2 :deep(.calendar-cell) {
         background: radial-gradient(
             circle,
             transparent 20%,
@@ -111,100 +88,57 @@
 
       // DIAGONAL
       #cell-3 {
-        grid-area: 1 / 8 / 3 / 8;
-        background: repeating-linear-gradient(
-          45deg,
-          $color-christmas-yellow,
-          $color-christmas-yellow 20px,
-          $color-christmas-yellow-dark 20px,
-          $color-christmas-yellow-dark 40px
-        );
-      }
-
-      // ZIGZAG
-      #cell-24 {
-        grid-area: 2 / 2 / 4 / 2;
-        background: linear-gradient(135deg, $color-christmas-red 25%, transparent 25%) -25px 0,
-          linear-gradient(225deg, $color-christmas-red 25%, transparent 25%) -25px 0,
-          linear-gradient(315deg, $color-christmas-red 25%, transparent 25%),
-          linear-gradient(45deg, $color-christmas-red 25%, transparent 25%);
-        background-size: 50px 50px;
-        background-color: $color-christmas-red-dark;
-        color: $color-white;
-      }
-
-      // STARS
-      #cell-8 {
-        grid-area: 2 / 9 / 4 / 9;
-        background: radial-gradient(
-            circle,
-            transparent 20%,
-            $color-christmas-green 20%,
-            $color-christmas-green 80%,
-            transparent 80%,
-            transparent
-          ),
-          radial-gradient(
-              circle,
-              transparent 20%,
-              $color-christmas-green 20%,
-              $color-christmas-green 80%,
-              transparent 80%,
-              transparent
-            )
-            30px 30px,
-          linear-gradient($color-christmas-yellow 2.4px, transparent 2.4px) 0 -1.2px,
-          linear-gradient(90deg, $color-christmas-yellow 2.4px, $color-christmas-green 2.4px) -1.2px 0,
-          $color-christmas-yellow;
-        background-size: 60px 60px, 60px 60px, 30px 30px, 30px 30px;
-        color: $color-background-light;
+        &.calendar-cell-container {
+          grid-area: 1 / 8 / 3 / 8;
+        }
+        :deep(.calendar-cell) {
+          background: repeating-linear-gradient(
+            45deg,
+            $color-christmas-yellow,
+            $color-christmas-yellow 20px,
+            $color-christmas-yellow-dark 20px,
+            $color-christmas-yellow-dark 40px
+          );
+        }
       }
 
       // STARS
       #cell-6 {
-        background: radial-gradient(
-            circle,
-            transparent 20%,
-            $color-background-light 20%,
-            $color-background-light 80%,
-            transparent 80%,
-            transparent
-          ),
-          radial-gradient(
+        &.calendar-cell-container {
+          grid-area: 3 / 3 / 3 / 5;
+        }
+        :deep(.calendar-cell) {
+          background: radial-gradient(
               circle,
               transparent 20%,
               $color-background-light 20%,
               $color-background-light 80%,
               transparent 80%,
               transparent
-            )
-            30px 30px,
-          linear-gradient($color-christmas-red 2.4px, transparent 2.4px) 0 -1.2px,
-          linear-gradient(90deg, $color-christmas-red 2.4px, $color-background-light 2.4px) -1.2px 0,
-          $color-christmas-red;
-        background-size: 60px 60px, 60px 60px, 30px 30px, 30px 30px;
-        color: $color-christmas-yellow;
-      }
-
-      // CENTERED ICON
-      #cell-13 {
-        background: $color-christmas-green;
-
-        :deep(.icon) {
+            ),
+            radial-gradient(
+                circle,
+                transparent 20%,
+                $color-background-light 20%,
+                $color-background-light 80%,
+                transparent 80%,
+                transparent
+              )
+              30px 30px,
+            linear-gradient($color-christmas-red 2.4px, transparent 2.4px) 0 -1.2px,
+            linear-gradient(90deg, $color-christmas-red 2.4px, $color-background-light 2.4px) -1.2px 0,
+            $color-christmas-red;
+          background-size: 60px 60px, 60px 60px, 30px 30px, 30px 30px;
           color: $color-christmas-yellow;
-
-          svg {
-            fill: $color-christmas-yellow;
-          }
         }
       }
 
       // CENTERED ICON
-      #cell-7 {
+      #cell-7 :deep(.calendar-cell) {
         background: $color-background-light;
         color: $color-white;
 
-        :deep(.icon) {
+        .icon {
           color: $color-background-very-light;
 
           svg {
@@ -213,57 +147,100 @@
         }
       }
 
-      #cell-23 {
-        position: relative;
-        align-items: flex-start;
-        justify-content: flex-start;
-        grid-area: 2 / 5 / 3 / 7;
-        background: $color-christmas-yellow;
-
-        :deep(.day) {
-          padding: 20px;
-          font-size: 45px;
+      // STARS
+      #cell-8 {
+        &.calendar-cell-container {
+          grid-area: 2 / 9 / 4 / 9;
         }
-
-        &::before {
-          content: '';
-          position: absolute;
-          bottom: 40px;
-          left: 0;
-          width: 100%;
-          height: 40px;
-          background: $color-christmas-red;
+        :deep(.calendar-cell) {
+          background: radial-gradient(
+              circle,
+              transparent 20%,
+              $color-christmas-green 20%,
+              $color-christmas-green 80%,
+              transparent 80%,
+              transparent
+            ),
+            radial-gradient(
+                circle,
+                transparent 20%,
+                $color-christmas-green 20%,
+                $color-christmas-green 80%,
+                transparent 80%,
+                transparent
+              )
+              30px 30px,
+            linear-gradient($color-christmas-yellow 2.4px, transparent 2.4px) 0 -1.2px,
+            linear-gradient(90deg, $color-christmas-yellow 2.4px, $color-christmas-green 2.4px) -1.2px 0,
+            $color-christmas-yellow;
+          background-size: 60px 60px, 60px 60px, 30px 30px, 30px 30px;
+          color: $color-background-light;
         }
+      }
 
-        &::after {
-          content: '';
-          position: absolute;
-          top: 0;
-          right: 40px;
-          width: 40px;
-          height: 100%;
-          background: $color-christmas-red;
+      // DIAGONAL
+      #cell-9 {
+        &.calendar-cell-container {
+          grid-area: 1 / 4 / 3 / 4;
+        }
+        :deep(.calendar-cell) {
+          background: repeating-linear-gradient(
+            -45deg,
+            $color-christmas-yellow,
+            $color-christmas-yellow 20px,
+            $color-christmas-green 20px,
+            $color-christmas-green 40px
+          );
         }
       }
 
       // ZIGZAG
-      #cell-20 {
-        grid-area: 2 / 7 / 4 / 7;
-        background: linear-gradient(135deg, $color-christmas-green 25%, transparent 25%) -25px 0,
-          linear-gradient(225deg, $color-christmas-green 25%, transparent 25%) -25px 0,
-          linear-gradient(315deg, $color-christmas-green 25%, transparent 25%),
-          linear-gradient(45deg, $color-christmas-green 25%, transparent 25%);
-        background-size: 50px 50px;
-        background-color: $color-christmas-green-dark;
+      #cell-10 {
+        &.calendar-cell-container {
+          grid-area: 1 / 6 / 1 / 8;
+        }
+        :deep(.calendar-cell) {
+          background: linear-gradient(135deg, $color-christmas-red 25%, transparent 25%) -25px 0,
+            linear-gradient(225deg, $color-christmas-red 25%, transparent 25%) -25px 0,
+            linear-gradient(315deg, $color-christmas-red 25%, transparent 25%),
+            linear-gradient(45deg, $color-christmas-red 25%, transparent 25%);
+          background-size: 50px 50px;
+          background-color: $color-christmas-red-dark;
+          color: $color-white;
+        }
+      }
+
+      // CENTERED ICON
+      #cell-13 :deep(.calendar-cell) {
+        background: $color-christmas-green;
+
+        .icon {
+          color: $color-christmas-yellow;
+
+          svg {
+            fill: $color-christmas-yellow;
+          }
+        }
+      }
+
+      // ICON
+      #cell-14 :deep(.calendar-cell) {
+        background-color: $color-christmas-yellow;
         color: $color-background-light;
       }
 
-      #cell-6 {
-        grid-area: 3 / 3 / 3 / 5;
+      // DOTS
+      #cell-15 :deep(.calendar-cell) {
+        background-color: $color-christmas-red;
+        background-image: radial-gradient($color-christmas-yellow-dark 12%, transparent 0),
+          radial-gradient($color-christmas-yellow-dark 12%, transparent 0);
+        background-size: 20px 20px;
+        background-position: 0 0, (20px / 2) (20px / 2);
+        color: $color-white;
       }
 
       // DOTS
-      #cell-16 {
+      #cell-16 :deep(.calendar-cell) {
         background-color: $color-background-light;
         background-image: radial-gradient($color-background-very-light 12%, transparent 0),
           radial-gradient($color-background-very-light 12%, transparent 0);
@@ -273,40 +250,99 @@
       }
 
       // ICON
-      #cell-17 {
+      #cell-17 :deep(.calendar-cell) {
         background-color: $color-background-light;
         color: $color-white;
       }
 
-      // ICON
-      #cell-22 {
-        background-color: $color-background-light;
-        color: $color-white;
+      // ZIGZAG
+      #cell-20 {
+        &.calendar-cell-container {
+          grid-area: 2 / 7 / 4 / 7;
+        }
+        :deep(.calendar-cell) {
+          background: linear-gradient(135deg, $color-christmas-green 25%, transparent 25%) -25px 0,
+            linear-gradient(225deg, $color-christmas-green 25%, transparent 25%) -25px 0,
+            linear-gradient(315deg, $color-christmas-green 25%, transparent 25%),
+            linear-gradient(45deg, $color-christmas-green 25%, transparent 25%);
+          background-size: 50px 50px;
+          background-color: $color-christmas-green-dark;
+          color: $color-background-light;
+        }
       }
 
       // DOTS
-      #cell-15 {
-        background-color: $color-christmas-red;
-        background-image: radial-gradient($color-christmas-yellow-dark 12%, transparent 0),
-          radial-gradient($color-christmas-yellow-dark 12%, transparent 0);
-        background-size: 20px 20px;
-        background-position: 0 0, (20px / 2) (20px / 2);
-        color: $color-white;
+      #cell-21 {
+        &.calendar-cell-container {
+          grid-area: 1 / 2 / 1 / 4;
+        }
+        :deep(.calendar-cell) {
+          background-color: $color-christmas-red;
+          background-image: radial-gradient($color-christmas-red-dark 12%, transparent 0),
+            radial-gradient($color-christmas-red-dark 12%, transparent 0);
+          background-size: 20px 20px;
+          background-position: 0 0, (20px / 2) (20px / 2);
+          color: $color-white;
+        }
       }
 
       // ICON
-      #cell-14 {
-        background-color: $color-christmas-yellow;
-        color: $color-background-light;
-      }
-
-      // CENTERED ICON
-      #cell-1 {
+      #cell-22 :deep(.calendar-cell) {
         background-color: $color-background-light;
         color: $color-white;
+      }
 
-        :deep(.centered-icon) {
-          color: $color-background-very-light;
+      // GIFT
+      #cell-23 {
+        &.calendar-cell-container {
+          grid-area: 2 / 5 / 3 / 7;
+        }
+        :deep(.calendar-cell) {
+          position: relative;
+          align-items: flex-start;
+          justify-content: flex-start;
+          background: $color-christmas-yellow;
+
+          .day {
+            padding: 20px;
+            font-size: 45px;
+          }
+
+          &::before {
+            content: '';
+            position: absolute;
+            bottom: 40px;
+            left: 0;
+            width: 100%;
+            height: 40px;
+            background: $color-christmas-red;
+          }
+
+          &::after {
+            content: '';
+            position: absolute;
+            top: 0;
+            right: 40px;
+            width: 40px;
+            height: 100%;
+            background: $color-christmas-red;
+          }
+        }
+      }
+
+      // ZIGZAG
+      #cell-24 {
+        &.calendar-cell-container {
+          grid-area: 2 / 2 / 4 / 2;
+        }
+        :deep(.calendar-cell) {
+          background: linear-gradient(135deg, $color-christmas-red 25%, transparent 25%) -25px 0,
+            linear-gradient(225deg, $color-christmas-red 25%, transparent 25%) -25px 0,
+            linear-gradient(315deg, $color-christmas-red 25%, transparent 25%),
+            linear-gradient(45deg, $color-christmas-red 25%, transparent 25%);
+          background-size: 50px 50px;
+          background-color: $color-christmas-red-dark;
+          color: $color-white;
         }
       }
     }
