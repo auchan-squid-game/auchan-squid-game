cf7c5eb3c19bc938c7035e00a7b4f0ed97b67a26
Florian Duquesne <fduquesne@icloud.com>
2021-10-21 17:28:00 +0200

    refactor: remove useless console.log

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,6 @@ import { HomepageView, AuthenticationView, LaunchCountdownView } from '../views'
 const launchAppDay = new Date('2021-12-25').getTime();
 const today = Date.now();
 const launched = launchAppDay - today < 0;
-console.log(launchAppDay - today);
 
 const routes = [
   { path: '/', component: launched ? HomepageView : LaunchCountdownView },
