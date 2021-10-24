455973ea32a291dfd5313768960b6d3e89b7697b
Florian Duquesne <fduquesne@icloud.com>
2021-10-25 00:15:22 +0200

    build: remove unused store getters

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,7 @@
 import { createStore } from 'vuex';
 
 import actions from './actions';
-import getters from './getters';
 import mutations from './mutations';
 import state from './state';
 
-export default createStore({ actions, getters, mutations, state });
+export default createStore({ actions, mutations, state });
