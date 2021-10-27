d5c55ba975667daf6dd7f1ba08f18d9dbdc6b6cb
Florian Duquesne <fduquesne@icloud.com>
2021-10-27 11:00:44 +0200

    refactor: add comments to user service methods

--- src/services/user-services.js
+++ src/services/user-services.js
@@ -4,12 +4,31 @@ import { get, ref, set } from 'firebase/database';
 import db from '@/config/firebase';
 
 export default {
+  /**
+   * Get all user data.
+   *
+   * @param {String} uid - user ID
+   * @returns Promise with all user data.
+   */
   get(uid) {
     return get(ref(db, `/users/${uid}`)).then(snap => snap.val());
   },
+
+  /**
+   * Logout the user.
+   *
+   * @returns Empty promise.
+   */
   logout() {
     return signOut(getAuth());
   },
+
+  /**
+   * Initialize user data (role, total points, accumulation of points), add user data in firebase and signup the user.
+   *
+   * @param {Object} user - user data (username, email, password)
+   * @returns Promise with user data stored in firebase.
+   */
   signup({ username, email, password }) {
     return createUserWithEmailAndPassword(getAuth(), email, password).then(userCredentials => {
       const uid = userCredentials.user.uid;
@@ -17,6 +36,13 @@ export default {
       return set(ref(db, `/users/${uid}`), user);
     });
   },
+
+  /**
+   * Signin the user.
+   *
+   * @param {Object} param0 - user data (email, password)
+   * @returns Empty promise.
+   */
   signin({ email, password }) {
     return signInWithEmailAndPassword(getAuth(), email, password);
   },
