0dc16a69552fec7d83c76ac506feb458af157785
Florian Duquesne <fduquesne@icloud.com>
2021-10-28 15:20:44 +0200

    feat(admin): add route to the administration page

---
+++ src/views/AdministationView/AdministationView.vue
@@ -0,0 +1,24 @@
+<template>
+  <Page shouldUserBeAuthenticated>
+    <div id="administration-page"></div>
+  </Page>
+</template>
+
+<script>
+  import Page from '@/layout/Page';
+
+  export default {
+    name: 'AdministationView',
+    components: { Page },
+    beforeMount() {
+      if (this.$store.state.user.role !== 'admin') this.$router.push('/');
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  #administration-page {
+    width: 100%;
+    height: 100%;
+  }
+</style>
