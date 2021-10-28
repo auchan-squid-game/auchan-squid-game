5c31eac9688826c16f074cbe53796bd40befa6bc
Florian Duquesne <fduquesne@icloud.com>
2021-10-28 15:09:07 +0200

    fix(homepage): show countdown only on production

--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,13 @@ import { AuthenticationView, PageNotFoundView, HomepageView, LaunchCountdownView
 const launchAppDay = new Date('2021-12-01').getTime();
 const today = Date.now();
 const launched = launchAppDay - today < 0;
+const showCountdown = launched && process.env.NODE_ENV !== 'develop';
 
 const routes = [
-  { path: '/', component: launched ? HomepageView : LaunchCountdownView },
+  { path: '/', component: showCountdown ? LaunchCountdownView : HomepageView },
   { path: '/login', component: AuthenticationView },
   { path: '/signup', component: AuthenticationView },
-  { path: '/ranking', component: launched ? RankingView : LaunchCountdownView },
+  { path: '/ranking', component: showCountdown ? LaunchCountdownView : RankingView },
   { path: '/:pathMatch(.*)*', component: PageNotFoundView },
 ];
 
