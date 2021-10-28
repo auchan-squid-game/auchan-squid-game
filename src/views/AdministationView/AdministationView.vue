e0b10f6ef8e52c58e0c9dcbe29ac9d405570e8a5
Florian Duquesne <fduquesne@icloud.com>
2021-10-28 16:07:27 +0200

    feat(admin): create enigma section component

--- src/views/AdministationView/AdministationView.vue
+++ src/views/AdministationView/AdministationView.vue
@@ -1,15 +1,18 @@
 <template>
   <Page shouldUserBeAuthenticated>
-    <div id="administration-page"></div>
+    <div id="administration-page">
+      <EnigmaSection :enigma="{ id: '1', title: 'TITRE DE L\'ENIGME 1' }" />
+    </div>
   </Page>
 </template>
 
 <script>
   import Page from '@/layout/Page';
+  import EnigmaSection from './EnigmaSection';
 
   export default {
     name: 'AdministationView',
-    components: { Page },
+    components: { Page, EnigmaSection },
     beforeMount() {
       if (this.$store.state.user.role !== 'admin') this.$router.push('/');
     },
@@ -18,6 +21,9 @@
 
 <style lang="scss" scoped>
   #administration-page {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     width: 100%;
     height: 100%;
   }
---
+++ src/views/AdministationView/EnigmaSection.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="enigma-section">
+    <div class="enigma-header">
+      <div class="enigma-title">#{{ enigma.id }} - {{ enigma.title }}</div>
+      <div class="enigma-toggle"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'EnigmaSection',
+    props: {
+      enigma: { type: Object, required: true },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  .enigma-section {
+    width: 900px;
+    height: 40px;
+    background: red;
+  }
+</style>
