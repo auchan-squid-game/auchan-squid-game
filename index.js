a09e7ac23b2125a6bca5ca07edb0394e9c1faf65
Amine YAMOUNI <amine.yamouni@gmail.com>
2021-11-19 15:27:31 +0100

    deploy auto

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const path = require('path');
 const serveStatic = require('serve-static');
 
 app.use('/', serveStatic(__dirname));
-app.get(/.*/, (_, res) => res.sendFile(path.join(__dirname, 'dist/index.html')));
+app.get(/.*/, (_, res) => res.sendFile(path.join(__dirname, './dist/index.html')));
 app.listen(process.env.PORT || 8080);
